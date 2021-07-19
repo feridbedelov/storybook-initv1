79ff70e82d6c56a300e8483f43dcab05b660e25d
feridbedelov <feridbedelov7@gmail.com>
2021-07-19 12:57:11 +0400

    test suites integrated with stories

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { Button } from '../index';
+import {
+  Large,
+  Medium,
+  Primary,
+  Secondary,
+  Small,
+  SmallSecondary,
+  SmallSecondaryWithCustomClass,
+} from '../Button.stories';
 
 describe('<Button /> component', () => {
   test('renders the button component correctly', () => {
@@ -14,64 +23,59 @@ describe('<Button /> component', () => {
   });
 
   test('renders the primary button by default', () => {
-    render(<Button label='Perfect Button' />);
+    render(<Primary {...Primary.args} />);
 
-    expect(screen.getByRole('button', { name: 'Perfect Button' })).toHaveClass(
+    expect(screen.getByRole('button', { name: 'Primary Button' })).toHaveClass(
       'button--primary',
     );
   });
 
   test('renders the secondary button', () => {
-    render(<Button label='Perfect Button' variant='secondary' />);
+    render(<Secondary {...Secondary.args} />);
 
-    expect(screen.getByRole('button', { name: 'Perfect Button' })).toHaveClass(
-      'button--secondary',
-    );
+    expect(
+      screen.getByRole('button', { name: 'Secondary Button' }),
+    ).toHaveClass('button--secondary');
   });
 
   test('renders the large button', () => {
-    render(<Button size='large' label='Perfect Button' variant='secondary' />);
+    render(<Large {...Large.args} />);
 
-    expect(screen.getByRole('button', { name: 'Perfect Button' })).toHaveClass(
+    expect(screen.getByRole('button', { name: 'Large Button' })).toHaveClass(
       'button--large',
     );
   });
 
   test('renders the medium button', () => {
-    render(<Button size='medium' label='Perfect Button' />);
+    render(<Medium {...Medium.args} />);
 
-    expect(screen.getByRole('button', { name: 'Perfect Button' })).toHaveClass(
+    expect(screen.getByRole('button', { name: 'Medium Button' })).toHaveClass(
       'button--medium',
     );
   });
 
   test('renders the small button', () => {
-    render(<Button size='small' label='Perfect Button' />);
+    render(<Small {...Small.args} />);
 
-    expect(screen.getByRole('button', { name: 'Perfect Button' })).toHaveClass(
+    expect(screen.getByRole('button', { name: 'Small Button' })).toHaveClass(
       'button--small',
     );
   });
 
   test('renders the small secondary button', () => {
-    render(<Button size='small' label='Perfect Button' variant='secondary' />);
+    render(<SmallSecondary {...SmallSecondary.args} />);
 
-    expect(screen.getByRole('button', { name: 'Perfect Button' })).toHaveClass(
-      'button--secondary button--small',
-    );
+    expect(
+      screen.getByRole('button', { name: 'Small Secondary Button' }),
+    ).toHaveClass('button--secondary button--small');
   });
 
   test('renders the small secondary button with custom className', () => {
     render(
-      <Button
-        size='small'
-        label='Perfect Button'
-        variant='secondary'
-        className='custom'
-      />,
+      <SmallSecondaryWithCustomClass {...SmallSecondaryWithCustomClass.args} />,
     );
 
-    expect(screen.getByRole('button', { name: 'Perfect Button' })).toHaveClass(
+    expect(screen.getByRole('button', { name: 'Custom Button' })).toHaveClass(
       'button--secondary button--small custom',
     );
   });
